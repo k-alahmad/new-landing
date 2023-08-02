dae2fb5c3a003032ab04afd43a9a331ab29300bb
Kanaan Alahmad <kanaan92@hotmail.com>
2023-08-02 20:00:10 +0400

    K-card-data-v1.0

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -1,109 +1,131 @@
 import pic330 from "../assets/images/home/330.webp";
 import picJlt from "../assets/images/home/jlt.webp";
 import picAmber from "../assets/images/home/amber.webp";
+import studio from "../assets/images/properties/studio.webp";
+import oneBR from "../assets/images/properties/1BR.webp";
+import twoBR from "../assets/images/properties/2BR.webp";
+import threeBR from "../assets/images/properties/3BR.webp";
 export const data = [
-  {
-    name: "330riverside",
-    image: pic330,
-    title: "Unveil A Soul-Stirring Experience",
-    description:
-      "As You Surrender To The Enchanting Call Of The Water In Your Vicinity",
-    plan: "20/60/20 %",
-    startingPrice: "1.2M AED",
-    url: "https://330riverside.avarealestate.ae/en",
-    location: "location",
-    handover: "handover",
-    propertes: [
-      {
-        startingPrice: "11",
-        area: "11",
-        bedroomNumber: "11",
-        bathsNumber: "11",
-        image: "11",
-      },
-      {
-        startingPrice: "12",
-        area: "12",
-        bedroomNumber: "12",
-        bathsNumber: "12",
-        image: "12",
-      },
-      {
-        startingPrice: "13",
-        area: "13",
-        bedroomNumber: "13",
-        bathsNumber: "13",
-        image: "13",
-      },
-    ],
-  },
-  {
-    name: "Me Do Re",
-    image: picJlt,
-    title: "Ready to Move Luxury Apartment",
-    description:
-      "Residents can wake up to the serene views of the water every morning. With a flexible post-handover payment plan option available, owning your dream home",
-    plan: "20/80 %",
-    startingPrice: "1.2M AED",
-    url: "https://jltproject.avarealestate.ae/",
-    location: "location",
-    handover: "handover",
-    propertes: [
-      {
-        startingPrice: "21",
-        area: "21",
-        bedroomNumber: "21",
-        bathsNumber: "21",
-        image: "21",
-      },
-      {
-        startingPrice: "22",
-        area: "22",
-        bedroomNumber: "22",
-        bathsNumber: "22",
-        image: "22",
-      },
-      {
-        startingPrice: "23",
-        area: "23",
-        bedroomNumber: "23",
-        bathsNumber: "23",
-        image: "23",
-      },
-    ],
-  },
-  {
-    name: "Amber",
-    image: picAmber,
-    title: "Living In Your Dream Home Isn't Far Away!",
-    description: "Move In Next Year.",
-    plan: "20/10/40/30 %",
-    startingPrice: "598K AED",
-    url: "https://amber.avarealestate.ae/",
-    location: "location",
-    handover: "handover",
-    propertes: [
-      {
-        startingPrice: "31",
-        area: "31",
-        bedroomNumber: "31",
-        bathsNumber: "31",
-        image: "31",
-      },
-      {
-        startingPrice: "32",
-        area: "32",
-        bedroomNumber: "32",
-        bathsNumber: "32",
-        image: "32",
-      },
-      {
-        startingPrice: "33",
-        area: "33",
-        bedroomNumber: "33",
-        bathsNumber: "33",
-        image: "33",
-      },
-    ],
-  },
+	{
+		name: "330riverside",
+		image: pic330,
+		title: "Unveil A Soul-Stirring Experience",
+		description:
+			"As You Surrender To The Enchanting Call Of The Water In Your Vicinity",
+		plan: {
+			name: "Post-handover",
+			onBooking: "20%",
+			duringConstruction: "60%",
+			onHandover: "20%",
+			postHandover: "",
+		},
+		startingPrice: "1.2M AED",
+		url: "https://330riverside.avarealestate.ae/en",
+		location: "location",
+		handover: "handover",
+		propertes: [
+			{
+				startingPrice: "AED1.5M",
+				area: "639.16 SQ.FT",
+				bedroomNumber: "1.5BR",
+				bathsNumber: "1",
+				image: oneBR,
+			},
+			{
+				startingPrice: "AED1.5M",
+				area: "748.84 SQ.FT",
+				bedroomNumber: "1BR",
+				bathsNumber: "1",
+				image: oneBR,
+			},
+			{
+				startingPrice: "AED1.5M",
+				area: "1024.29 SQ.FT",
+				bedroomNumber: "2BR",
+				bathsNumber: "1",
+				image: twoBR,
+			},
+		],
+	},
+	{
+		name: "Me Do Re",
+		image: picJlt,
+		title: "Ready to Move Luxury Apartment",
+		description:
+			"Residents can wake up to the serene views of the water every morning. With a flexible post-handover payment plan option available, owning your dream home",
+		plan: {
+			name: "Post-handover",
+			onBooking: "",
+			duringConstruction: "",
+			onHandover: "",
+			postHandover: "",
+		},
+		startingPrice: "1.2M AED",
+		url: "https://jltproject.avarealestate.ae/",
+		location: "location",
+		handover: "handover",
+		propertes: [
+			{
+				startingPrice: "21",
+				area: "21",
+				bedroomNumber: "21",
+				bathsNumber: "21",
+				image: "21",
+			},
+			{
+				startingPrice: "22",
+				area: "22",
+				bedroomNumber: "22",
+				bathsNumber: "22",
+				image: "22",
+			},
+			{
+				startingPrice: "23",
+				area: "23",
+				bedroomNumber: "23",
+				bathsNumber: "23",
+				image: "23",
+			},
+		],
+	},
+	{
+		name: "Amber",
+		image: picAmber,
+		title: "Living In Your Dream Home Isn't Far Away!",
+		description: "Move In Next Year.",
+		plan: {
+			name: "Post-handover",
+			onBooking: "",
+			duringConstruction: "",
+			onHandover: "",
+			postHandover: "",
+		},
+		startingPrice: "598K AED",
+		url: "https://amber.avarealestate.ae/",
+		location: "location",
+		handover: "handover",
+		propertes: [
+			{
+				startingPrice: "31",
+				area: "31",
+				bedroomNumber: "31",
+				bathsNumber: "31",
+				image: "31",
+			},
+			{
+				startingPrice: "32",
+				area: "32",
+				bedroomNumber: "32",
+				bathsNumber: "32",
+				image: "32",
+			},
+			{
+				startingPrice: "33",
+				area: "33",
+				bedroomNumber: "33",
+				bathsNumber: "33",
+				image: "33",
+			},
+		],
+	},
 ];
