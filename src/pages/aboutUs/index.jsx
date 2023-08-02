65bfa87f7ffb2f730205a2db7a46759fd56b92fe
Kanaan Alahmad <kanaan92@hotmail.com>
2023-08-02 18:46:18 +0400

    K-aboutus-v1.0

--- src/pages/aboutUs/index.jsx
+++ src/pages/aboutUs/index.jsx
@@ -1,7 +1,17 @@
 import React from "react";
-
+import kasra from "../../assets/images/aboutus/kasra.png";
+import maryam from "../../assets/images/aboutus/maryam.png";
+import dubai from "../../assets/images/aboutus/dubai.webp";
 const AboutUs = () => {
-  return <div className="mt-28">AboutUss</div>;
+	return (
+		<div className='mt-28 w-[100%] place-self-center px-12'>
+			<div className='flex  justify-center bg-blue-950'>
+				<p className='text-[#AA8A3A] text-4xl shadow-inner font-extrabold py-4'>
+					AVA REAL ESTATE
+				</p>
+			</div>
+		</div>
+	);
 };
 
 export default AboutUs;
