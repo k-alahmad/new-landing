730eb22b01952db4cb6afd65ec26a702a14874a9
moazabdulhalim <moazabdulhalim@gmail.com>
2023-08-02 18:59:57 +0300

    propertoes v1.0

--- src/components/UI/Loader/index.jsx
+++ src/components/UI/Loader/index.jsx
@@ -2,48 +2,14 @@ import React from "react";
 import "./Loader.css";
 
 const Loader = ({ big }) => {
-  return (
-    <div className="container">
-      <div className="tree">
-        {big && (
-          <>
-            <div className="branch" style={{ "--x": 0 }}>
-              <span style={{ "--i": 0 }}></span>
-              <span style={{ "--i": 1 }}></span>
-              <span style={{ "--i": 2 }}></span>
-              <span style={{ "--i": 3 }}></span>
-            </div>
-            <div className="branch" style={{ "--x": 1 }}>
-              <span style={{ "--i": 0 }}></span>
-              <span style={{ "--i": 1 }}></span>
-              <span style={{ "--i": 2 }}></span>
-              <span style={{ "--i": 3 }}></span>
-            </div>
-          </>
-        )}
-        <div className="branch" style={{ "--x": big ? 2 : 0 }}>
-          <span style={{ "--i": 0 }}></span>
-          <span style={{ "--i": 1 }}></span>
-          <span style={{ "--i": 2 }}></span>
-          <span style={{ "--i": 3 }}></span>
-        </div>
-        <div className="branch" style={{ "--x": big ? 3 : 0.3 }}>
-          <span style={{ "--i": 0 }}></span>
-          <span style={{ "--i": 1 }}></span>
-          <span style={{ "--i": 2 }}></span>
-          <span style={{ "--i": 3 }}></span>
-        </div>
-        <div className="stem">
-          <span style={{ "--i": 0 }}></span>
-          <span style={{ "--i": 1 }}></span>
-          <span style={{ "--i": 2 }}></span>
-          <span style={{ "--i": 3 }}></span>
-        </div>
-        <span className="shadow"></span>
-      </div>
-      {/* <p className="text-smaller mt-10 font-fmed">LOADING..</p> */}
-    </div>
-  );
+	return (
+		<div className='container'>
+			<span></span>
+			<span></span>
+			<span></span>
+			<span></span>
+		</div>
+	);
 };
 
 export default Loader;
