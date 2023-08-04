d4f1d0b66c7eeddcb0b859a41f037a05f033c7c6
moazabdulhalim <moazabdulhalim@gmail.com>
2023-08-04 13:13:33 +0300

    about us data file

---
+++ src/data/aboutUsData.js
@@ -0,0 +1,69 @@
+import kasra from "../assets/images/aboutus/kasra.webp";
+import maryam from "../assets/images/aboutus/maryam.webp";
+import dubai from "../assets/images/aboutus/dubai.webp";
+import dubaiImageIcon from "../assets/icons/about.svg";
+import teamWorkIcon from "../assets/icons/teamWork.svg";
+import projectsIcon from "../assets/icons/projects.svg";
+import facesIcon from "../assets/icons/faces.svg";
+import developmentIcon from "../assets/icons/development.svg";
+import Logo from "../assets/logos/black-logo.svg";
+export const data = {
+  headerTitle: "Get to know us closely and what we excel at",
+  mission: {
+    title: "Mission",
+    description:
+      "Lorem ipsum dolor sit amet . The graphic and typographic operators know this well, in reality all the professions dealing with the universe of communication have a stable relationship Lorem ipsum dolor sit amet . The graphic and typographic operators know this well, in reality all the professions dealing with the universe of communication",
+    image: dubai,
+    items: [
+      {
+        icon: teamWorkIcon,
+        title: "Team Work",
+        description:
+          "Lorem ipsum dolor sit amet . The graphic and typographic operators know this well, in reality all the professions dealing with the universe of communication have a stable relationship Lorem ipsum dolor sit amet . The graphic and typographic operators know this well, in reality all the professions dealing with the universe of communication",
+      },
+      {
+        icon: projectsIcon,
+        title: "Projects",
+        description:
+          "Lorem ipsum dolor sit amet . The graphic and typographic operators know this well, in reality all the professions dealing with the universe of communication have a stable relationship Lorem ipsum dolor sit amet . The graphic and typographic operators know this well, in reality all the professions dealing with the universe of communication",
+      },
+      {
+        icon: developmentIcon,
+        title: "Development",
+        description:
+          "Lorem ipsum dolor sit amet . The graphic and typographic operators know this well, in reality all the professions dealing with the universe of communication have a stable relationship Lorem ipsum dolor sit amet . The graphic and typographic operators know this well, in reality all the professions dealing with the universe of communication",
+      },
+      {
+        icon: facesIcon,
+        title: "Projects",
+        description:
+          "Lorem ipsum dolor sit amet . The graphic and typographic operators know this well, in reality all the professions dealing with the universe of communication have a stable relationship Lorem ipsum dolor sit amet . The graphic and typographic operators know this well, in reality all the professions dealing with the universe of communication",
+      },
+    ],
+  },
+  vision: {
+    title: "Vision",
+    description:
+      "We work with a clear and thoughtful vision to achieve what we aspire to",
+    imageOne: dubai,
+    imageTwo: dubai,
+    imageThree: dubai,
+    items: [
+      {
+        text: "Lorem ipsum dolor sit amet . The graphic and typographic operators know this well, in reality all the professions dealing with the universe of communication have a stable relationship Lorem ipsum dolor sit amet . The graphic and typographic operators know this well, in reality all the professions dealing with the universe of communication",
+      },
+      {
+        text: "Lorem ipsum dolor sit amet . The graphic and typographic operators know this well, in reality all the professions dealing with the universe of communication have a stable relationship Lorem ipsum dolor sit amet . The graphic and typographic operators know this well, in reality all the professions dealing with the universe of communication",
+      },
+      {
+        text: "Lorem ipsum dolor sit amet . The graphic and typographic operators know this well, in reality all the professions dealing with the universe of communication have a stable relationship Lorem ipsum dolor sit amet . The graphic and typographic operators know this well, in reality all the professions dealing with the universe of communication",
+      },
+      {
+        text: "Lorem ipsum dolor sit amet . The graphic and typographic operators know this well, in reality all the professions dealing with the universe of communication have a stable relationship Lorem ipsum dolor sit amet . The graphic and typographic operators know this well, in reality all the professions dealing with the universe of communication",
+      },
+      {
+        text: "Lorem ipsum dolor sit amet . The graphic and typographic operators know this well, in reality all the professions dealing with the universe of communication have a stable relationship Lorem ipsum dolor sit amet . The graphic and typographic operators know this well, in reality all the professions dealing with the universe of communication",
+      },
+    ],
+  },
+};
--- src/pages/aboutUs/index.jsx
+++ src/pages/aboutUs/index.jsx
@@ -9,7 +9,7 @@ import projectsIcon from "../../assets/icons/projects.svg";
 import facesIcon from "../../assets/icons/faces.svg";
 import developmentIcon from "../../assets/icons/development.svg";
 import Logo from "../../assets/logos/black-logo.svg";
-
+import { data } from "../../data/aboutUsData";
 const AboutUs = () => {
   const MissionCard = ({ title, text, icon }) => {
     return (
@@ -26,14 +26,7 @@ const AboutUs = () => {
           </div>
           <p className="font-semibold text-small">{title}</p>
         </div>
-        <p className="text-tiny">
-          {/* {text} */}
-          Lorem ipsum dolor sit amet . The graphic and typographic operators
-          know this well, in reality all the professions dealing with the
-          universe of communication have a stable relationship Lorem ipsum dolor
-          sit amet . The graphic and typographic operators know this well, in
-          reality all the professions dealing with the universe of communication
-        </p>
+        <p className="text-tiny">{text}</p>
       </div>
     );
   };
@@ -42,7 +35,7 @@ const AboutUs = () => {
     <div className="mt-">
       <div className="xl:h-[500px] 2xl:h-[600px] flex justify-center items-center bg-headerBg bg-secondary bg-no-repeat bg-cover bg-center">
         <GradientText
-          text={"Get To Know Us Closely And What We Excel At"}
+          text={data.headerTitle}
           customStyle={
             "text-center text-big 2xl:text-bigger font-bold text-primary w-[50%]"
           }
@@ -56,7 +49,11 @@ const AboutUs = () => {
             }}
             className="h-[140px] w-[140px] rounded-md rounded-tr-full backdrop-blur-[15px] absolute -right-[10%] -top-[10%]"
           />
-          <img src={dubai} alt="" className="h-[600px] rounded-md" />
+          <img
+            src={data.mission.image}
+            alt=""
+            className="h-[600px] rounded-md"
+          />
           <div
             style={{
               background:
@@ -74,72 +71,31 @@ const AboutUs = () => {
           </div>
         </div>
         <div className="col-span-7 space-y-4">
-          <p className="text-bigger font-bold"> Mission</p>
-          <p>
-            Lorem ipsum dolor sit amet . The graphic and typographic operators
-            know this well, in reality all the professions dealing with the
-            universe of communication have a stable relationship Lorem ipsum
-            dolor sit amet . The graphic and typographic operators know this
-            well, in reality all the professions dealing with the universe of
-            communication
-          </p>
+          <p className="text-bigger font-bold"> {data.mission.title} </p>
+          <p>{data.mission.description}</p>
           <div className="grid grid-cols-2 gap-7">
-            <MissionCard title={"Team Work"} icon={teamWorkIcon} />
-            <MissionCard title={"Projects"} icon={projectsIcon} />
-            <MissionCard title={"Development"} icon={developmentIcon} />
-            <MissionCard title={"Projects"} icon={facesIcon} />
+            {data.mission.items.map((item, index) => {
+              return (
+                <MissionCard
+                  key={index}
+                  title={item.title}
+                  icon={item.icon}
+                  text={item.description}
+                />
+              );
+            })}
           </div>
         </div>
       </div>
 
       <div className="grid grid-cols-12 gap-7  mt-20">
         <div className="col-span-6 w-[80%] relative place-self-center space-y-4">
-          <p className="text-bigger font-bold"> Vision</p>
-          <p className="text-small">
-            We work with a clear and thoughtful vision to achieve what we aspire
-            to
-          </p>
+          <p className="text-bigger font-bold"> {data.vision.title} </p>
+          <p className="text-small">{data.vision.description}</p>
           <ul className="list-disc ml-7 space-y-14 marker:text-primary list-outside pt-12">
-            <li>
-              Lorem ipsum dolor sit amet . The graphic and typographic operators
-              know this well, in reality all the professions dealing with the
-              universe of communication have a stable relationship Lorem ipsum
-              dolor sit amet . The graphic and typographic operators know this
-              well, in reality all the professions dealing with the universe of
-              communication
-            </li>
-            <li>
-              Lorem ipsum dolor sit amet . The graphic and typographic operators
-              know this well, in reality all the professions dealing with the
-              universe of communication have a stable relationship Lorem ipsum
-              dolor sit amet . The graphic and typographic operators know this
-              well, in reality all the professions dealing with the universe of
-              communication
-            </li>
-            <li>
-              Lorem ipsum dolor sit amet . The graphic and typographic operators
-              know this well, in reality all the professions dealing with the
-              universe of communication have a stable relationship Lorem ipsum
-              dolor sit amet . The graphic and typographic operators know this
-              well, in reality all the professions dealing with the universe of
-              communication
-            </li>
-            <li>
-              Lorem ipsum dolor sit amet . The graphic and typographic operators
-              know this well, in reality all the professions dealing with the
-              universe of communication have a stable relationship Lorem ipsum
-              dolor sit amet . The graphic and typographic operators know this
-              well, in reality all the professions dealing with the universe of
-              communication
-            </li>
-            <li>
-              Lorem ipsum dolor sit amet . The graphic and typographic operators
-              know this well, in reality all the professions dealing with the
-              universe of communication have a stable relationship Lorem ipsum
-              dolor sit amet . The graphic and typographic operators know this
-              well, in reality all the professions dealing with the universe of
-              communication
-            </li>
+            {data.vision.items.map((item, index) => {
+              return <li key={index}>{item.text}</li>;
+            })}
           </ul>
         </div>
         <div className="col-span-6 w-[80%] place-items-center space-y-7 relative">
@@ -156,9 +112,21 @@ const AboutUs = () => {
 
             <img src={Logo} alt="" className="h-[135px] w-[185px]" />
           </div>
-          <img src={dubai} className="h-[320px] w-full rounded-md" alt="" />
-          <img src={dubai} className="h-[320px] w-[40%] rounded-md" alt="" />
-          <img src={dubai} className="h-[320px] w-full rounded-md" alt="" />
+          <img
+            src={data.vision.imageOne}
+            className="h-[320px] w-full rounded-md"
+            alt=""
+          />
+          <img
+            src={data.vision.imageTwo}
+            className="h-[320px] w-[40%] rounded-md"
+            alt=""
+          />
+          <img
+            src={data.vision.imageThree}
+            className="h-[320px] w-full rounded-md"
+            alt=""
+          />
         </div>
       </div>
       <br />
