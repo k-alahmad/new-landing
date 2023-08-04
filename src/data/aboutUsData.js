9034c97746aba145670345d7a19bdd689056d35b
Kanaan Alahmad <kanaan92@hotmail.com>
2023-08-04 18:10:54 +0400

    K-Data-v1.0

--- src/data/aboutUsData.js
+++ src/data/aboutUsData.js
@@ -8,62 +8,59 @@ import facesIcon from "../assets/icons/faces.svg";
 import developmentIcon from "../assets/icons/development.svg";
 import Logo from "../assets/logos/black-logo.svg";
 export const data = {
-  headerTitle: "Get to know us closely and what we excel at",
-  mission: {
-    title: "Mission",
-    description:
-      "Lorem ipsum dolor sit amet . The graphic and typographic operators know this well, in reality all the professions dealing with the universe of communication have a stable relationship Lorem ipsum dolor sit amet . The graphic and typographic operators know this well, in reality all the professions dealing with the universe of communication",
-    image: dubai,
-    items: [
-      {
-        icon: teamWorkIcon,
-        title: "Team Work",
-        description:
-          "Lorem ipsum dolor sit amet . The graphic and typographic operators know this well, in reality all the professions dealing with the universe of communication have a stable relationship Lorem ipsum dolor sit amet . The graphic and typographic operators know this well, in reality all the professions dealing with the universe of communication",
-      },
-      {
-        icon: projectsIcon,
-        title: "Projects",
-        description:
-          "Lorem ipsum dolor sit amet . The graphic and typographic operators know this well, in reality all the professions dealing with the universe of communication have a stable relationship Lorem ipsum dolor sit amet . The graphic and typographic operators know this well, in reality all the professions dealing with the universe of communication",
-      },
-      {
-        icon: developmentIcon,
-        title: "Development",
-        description:
-          "Lorem ipsum dolor sit amet . The graphic and typographic operators know this well, in reality all the professions dealing with the universe of communication have a stable relationship Lorem ipsum dolor sit amet . The graphic and typographic operators know this well, in reality all the professions dealing with the universe of communication",
-      },
-      {
-        icon: facesIcon,
-        title: "Projects",
-        description:
-          "Lorem ipsum dolor sit amet . The graphic and typographic operators know this well, in reality all the professions dealing with the universe of communication have a stable relationship Lorem ipsum dolor sit amet . The graphic and typographic operators know this well, in reality all the professions dealing with the universe of communication",
-      },
-    ],
-  },
-  vision: {
-    title: "Vision",
-    description:
-      "We work with a clear and thoughtful vision to achieve what we aspire to",
-    imageOne: dubai,
-    imageTwo: dubai,
-    imageThree: dubai,
-    items: [
-      {
-        text: "Lorem ipsum dolor sit amet . The graphic and typographic operators know this well, in reality all the professions dealing with the universe of communication have a stable relationship Lorem ipsum dolor sit amet . The graphic and typographic operators know this well, in reality all the professions dealing with the universe of communication",
-      },
-      {
-        text: "Lorem ipsum dolor sit amet . The graphic and typographic operators know this well, in reality all the professions dealing with the universe of communication have a stable relationship Lorem ipsum dolor sit amet . The graphic and typographic operators know this well, in reality all the professions dealing with the universe of communication",
-      },
-      {
-        text: "Lorem ipsum dolor sit amet . The graphic and typographic operators know this well, in reality all the professions dealing with the universe of communication have a stable relationship Lorem ipsum dolor sit amet . The graphic and typographic operators know this well, in reality all the professions dealing with the universe of communication",
-      },
-      {
-        text: "Lorem ipsum dolor sit amet . The graphic and typographic operators know this well, in reality all the professions dealing with the universe of communication have a stable relationship Lorem ipsum dolor sit amet . The graphic and typographic operators know this well, in reality all the professions dealing with the universe of communication",
-      },
-      {
-        text: "Lorem ipsum dolor sit amet . The graphic and typographic operators know this well, in reality all the professions dealing with the universe of communication have a stable relationship Lorem ipsum dolor sit amet . The graphic and typographic operators know this well, in reality all the professions dealing with the universe of communication",
-      },
-    ],
-  },
+	headerTitle: "Get to know us closely and what we excel at",
+	mission: {
+		title: "Mission",
+		description:
+			"our mission is to provide exceptional professional services to our clients with integrity, transparency, and excellence.",
+		image: dubai,
+		items: [
+			{
+				icon: teamWorkIcon,
+				title: "Trust",
+				description:
+					"We are dedicated to providing a positive experience and to earning the trust and confidence of our clients. We strive to deliver on our promises and to achieve the best possible outcomes for our clients.",
+			},
+			{
+				icon: projectsIcon,
+				title: "Leading",
+				description:
+					"We aim to be recognized as a leading real estate company in Dubai by offering a comprehensive range of services such as buying, selling, renting, and managing properties. We ensure that our clients’ needs are met with the utmost diligence and attention to detail.",
+			},
+			{
+				icon: developmentIcon,
+				title: "Solutions",
+				description:
+					"Our mission is to provide customized solutions to our clients and to exceed their expectations. ",
+			},
+			{
+				icon: facesIcon,
+				title: "Diversity",
+				description:
+					"We strive to meet the diverse needs of our clients and to establish a lasting relationship based on trust and mutual respect.",
+			},
+		],
+	},
+	vision: {
+		title: "Vision",
+		description:
+			"At AVA Real Estate, our vision is to be The Best Real Estate Company in the industry",
+		imageOne: dubai,
+		imageTwo: dubai,
+		imageThree: dubai,
+		items: [
+			{
+				text: "Both in the minds of people and the communities. We expect and believe in delivering The Best we have to offer by always keeping our values at the top without compromising customer values.",
+			},
+			{
+				text: "We want to create The Best working environment where everyone has equal opportunity to grow and be proud of working together for the growth of the communities and the nation.",
+			},
+			{
+				text: "|Our vision is supported by two fundamental principles that provide the foundation of all our activities: Organizational Excellence and the Customer Values.",
+			},
+			{
+				text: "Our vision tells the world-what we care about; our clients, communities and the employees. We believe in building life-long relationships with Our clients.",
+			},
+		],
+	},
 };
