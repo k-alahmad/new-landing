0101a0e9e4ab67c90e145c2a243f6959fd908b91
Kanaan Alahmad <kanaan92@hotmail.com>
2023-08-18 12:06:07 +0400

    K-data-v1.0

--- src/pages/home/components/About/index.jsx
+++ src/pages/home/components/About/index.jsx
@@ -3,33 +3,35 @@ import dubaiImage from "../../../../assets/images/aboutus/dubai.webp";
 import logo from "../../../../assets/logos/white-logo.svg";
 
 const About = () => {
-  return (
-    <div className="py-24 px-[5%] rounded-md ">
-      <div
-        style={{
-          backgroundImage: `url(${dubaiImage})`,
-        }}
-        className="rounded-md bg-no-repeat bg-cover bg-center md:h-[500px]"
-      >
-        <div className="bg-black/50 h-full p-[5%]">
-          <img src={logo} alt="AVA Logo" className="w-32 h-32" />
-          <div className="md:grid md:grid-cols-2 place-items-center text-white py-10">
-            <p className="font-bold text-big ">
-              All you need about Dubai apartments, in one place
-            </p>
-            <p>
-              Lorem ipsum dolor sit amet . The graphic and typographic operators
-              know this well, in reality all the professions dealing with the
-              universe of communication have a stable relationship Lorem ipsum
-              dolor sit amet . The graphic and typographic operators know this
-              well, in reality all the professions dealing with the universe of
-              communication
-            </p>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div className='py-24 px-[5%] rounded-md '>
+			<div
+				style={{
+					backgroundImage: `url(${dubaiImage})`,
+				}}
+				className='rounded-md bg-no-repeat bg-cover bg-center md:h-[500px]'
+			>
+				<div className='bg-black/50 h-full p-[5%]'>
+					<img src={logo} alt='AVA Logo' className='w-32 h-32' />
+					<div className='md:grid md:grid-cols-2 place-items-center text-white py-10'>
+						<p className='font-bold text-big '>
+							All you need about Dubai apartments, in one place
+						</p>
+						<p>
+							At AVA Real Estate, our vision is to be The Best Real Estate
+							Company in the industry. Both in the minds of people and the
+							communities. We expect and believe in delivering The Best we have
+							to offer by always keeping our values at the top without
+							compromising customer values. We want to create The Best working
+							environment where everyone has equal opportunity to grow and be
+							proud of working together for the growth of the communities and
+							the nation.
+						</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default About;
