5fab539de302634eeb3bfd3c34bdec0edb1a0223
Kanaan Alahmad <kanaan92@hotmail.com>
2023-08-24 13:35:45 +0400

    K-Articles-SEO-v1.1

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -3,19 +3,36 @@ import Header from "./components/Header/Header";
 import ContactUs from "./components/ContactUs";
 import Properties from "./components/Properties";
 import About from "./components/About";
-
+import { Helmet } from "react-helmet";
 const HomePage = () => {
-  useEffect(() => {
-    window.scrollTo(0, 0);
-  }, []);
-  return (
-    <div>
-      <Header />
-      <Properties />
-      <ContactUs />
-      <About />
-    </div>
-  );
+	useEffect(() => {
+		window.scrollTo(0, 0);
+	}, []);
+	return (
+		<div>
+			<Helmet htmlAttributes>
+				<html lang='en' />
+				<meta name='title' content='Ava Real Estate' />
+				<meta
+					name='description'
+					content='Explore premium properties in Dubai with AVA Real Estate, your trusted brokerage agency. Discover exquisite homes, apartments, and commercial spaces for buy or rent. Our expert agents ensure a seamless real estate experience, guiding you through the vibrant Dubai market.'
+				/>
+				<meta
+					name='keywords'
+					content='Dubai Real Estate, Brokerage Agency, Property Listings, Homes for Sale, Apartments for Rent, Commercial Spaces, Expert Agents, Dubai Property Market, AVA Real Estate Dubai, offplan, Dubai properties, properties for sale Dubai, apartements for sale duabi, best real estate agency, buy apartment in dubai, buy home in dubai, buy home with installment, home investment, invest in properties Dubai'
+				/>
+				<link
+					rel='icon'
+					type='image/svg+xml'
+					href='./src/assets/logos/AVA-Logo.svg'
+				/>
+			</Helmet>
+			<Header />
+			<Properties />
+			<ContactUs />
+			<About />
+		</div>
+	);
 };
 
 export default HomePage;
