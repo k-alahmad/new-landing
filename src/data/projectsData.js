5277618cc908113c470086044949de03e161e2bf
Kanaan Alahmad <kanaan92@hotmail.com>
2023-12-14 19:08:38 +0400

    K-Data-v1.0

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -1,75 +1,85 @@
 import projectOneHeader from "../assets/images/ghaf-header.webp";
+import projectTwoHeader from "../assets/images/acres.webp";
 import locationIcon from "../assets/icons/location-icon.svg";
 import coinIcon from "../assets/icons/coin.svg";
 export const projectsData = [
-  {
-    id: 1,
-    ProjectName: { en: "Tilal Al Ghaf", ar: "" },
-    HeaderTitle: { en: "Ultra Luxury Villa at Tillal Al Ghaf", ar: "" },
-    HeaderImage: projectOneHeader,
-    InfoTitle: { en: "Ultra Luxury Villa at Tillal Al Ghaf", ar: "" },
-    InfoSubTitle: {
-      en: "Enjoy panoramic views of the pristine Lagoon AL Ghaf as you indulge in a truly luxurios lifestyle",
-      ar: "",
-    },
-    InfoData: [
-      {
-        Icon: coinIcon,
-        Title: { en: "AED24.6M", ar: "" },
-        SubTitle: { en: "startingPrice", ar: "" },
-      },
-      {
-        Icon: locationIcon,
-        Title: { en: "Hessa Street", ar: "" },
-        SubTitle: { en: "Location", ar: "" },
-      },
-    ],
-    Nearby: [
-      {
-        Duration: "Next To",
-        Place: { en: "Butterfly Garnden", ar: "" },
-        Image: "",
-      },
-      { Duration: "15 Min", Place: { en: "Dubai Marina", ar: "" }, Image: "" },
-      {
-        Duration: "10 Min",
-        Place: { en: "Dubai Hills Mall", ar: "" },
-        Image: "",
-      },
-      { Duration: "29 Min", Place: { en: "DXB Airport", ar: "" }, Image: "" },
-      {
-        Duration: "20 Min",
-        Place: { en: "Dubai Downtown", ar: "" },
-        Image: "",
-      },
-      {
-        Duration: "15    Min",
-        Place: { en: "Mall of Emirates", ar: "" },
-        Image: "",
-      },
-    ],
-    Video: "",
-  },
-  {
-    id: 2,
-    ProjectName: { en: "Acres", ar: "" },
-    HeaderTitle: { en: "", ar: "" },
-    HeaderImage: "",
-    InfoTitle: { en: "", ar: "" },
-    InfoSubTitle: { en: "", ar: "" },
-    InfoData: [
-      { Icon: "", Title: { en: "", ar: "" }, SubTitle: { en: "", ar: "" } },
-      { Icon: "", Title: { en: "", ar: "" }, SubTitle: { en: "", ar: "" } },
-      { Icon: "", Title: { en: "", ar: "" }, SubTitle: { en: "", ar: "" } },
-    ],
-    Nearby: [
-      { Duration: "", Place: { en: "", ar: "" }, Image: "" },
-      { Duration: "", Place: { en: "", ar: "" }, Image: "" },
-      { Duration: "", Place: { en: "", ar: "" }, Image: "" },
-      { Duration: "", Place: { en: "", ar: "" }, Image: "" },
-      { Duration: "", Place: { en: "", ar: "" }, Image: "" },
-      { Duration: "", Place: { en: "", ar: "" }, Image: "" },
-    ],
-    Video: "",
-  },
+	{
+		id: 1,
+		ProjectName: { en: "Tilal Al Ghaf", ar: "تلال الغاف" },
+		HeaderTitle: {
+			en: "Ultra Luxury Villa at Tillal Al Ghaf",
+			ar: "فلل فاخرة جدا من تلال الغاف",
+		},
+		HeaderImage: projectOneHeader,
+		InfoTitle: {
+			en: "Ultra Luxury Villa at Tillal Al Ghaf",
+			ar: "فلل فاخرة جدا من تلال الغاف",
+		},
+		InfoSubTitle: {
+			en: "Enjoy panoramic views of the pristine Lagoon AL Ghaf as you indulge in a truly luxurious lifestyle",
+			ar: "استمتع بإطلالات بانورامية على بحيرة الغاف البكر بينما تنغمس في أسلوب حياة فاخر حقًا",
+		},
+		InfoData: [
+			{
+				Icon: coinIcon,
+				Title: { en: "AED24.6M", ar: "AED24.6M" },
+				SubTitle: { en: "Starting Price", ar: "بأسعار تبدأ من" },
+			},
+			{
+				Icon: locationIcon,
+				Title: { en: "Hessa Street", ar: "شارع حصة" },
+				SubTitle: { en: "Location", ar: "الموقع" },
+			},
+		],
+		Nearby: [
+			{
+				Duration: "Next To",
+				Place: { en: "Butterfly Garnden", ar: "" },
+				Image: "",
+			},
+			{ Duration: "15 Min", Place: { en: "Dubai Marina", ar: "" }, Image: "" },
+			{
+				Duration: "10 Min",
+				Place: { en: "Dubai Hills Mall", ar: "" },
+				Image: "",
+			},
+			{ Duration: "29 Min", Place: { en: "DXB Airport", ar: "" }, Image: "" },
+			{
+				Duration: "20 Min",
+				Place: { en: "Dubai Downtown", ar: "" },
+				Image: "",
+			},
+			{
+				Duration: "15    Min",
+				Place: { en: "Mall of Emirates", ar: "" },
+				Image: "",
+			},
+		],
+		Video: "",
+	},
+	{
+		id: 2,
+		ProjectName: { en: "Acres", ar: "" },
+		HeaderTitle: {
+			en: "The Acres, every step inside brings you closer to the outdoors.",
+			ar: "",
+		},
+		HeaderImage: projectTwoHeader,
+		InfoTitle: { en: "", ar: "" },
+		InfoSubTitle: { en: "", ar: "" },
+		InfoData: [
+			{ Icon: "", Title: { en: "", ar: "" }, SubTitle: { en: "", ar: "" } },
+			{ Icon: "", Title: { en: "", ar: "" }, SubTitle: { en: "", ar: "" } },
+			{ Icon: "", Title: { en: "", ar: "" }, SubTitle: { en: "", ar: "" } },
+		],
+		Nearby: [
+			{ Duration: "", Place: { en: "", ar: "" }, Image: "" },
+			{ Duration: "", Place: { en: "", ar: "" }, Image: "" },
+			{ Duration: "", Place: { en: "", ar: "" }, Image: "" },
+			{ Duration: "", Place: { en: "", ar: "" }, Image: "" },
+			{ Duration: "", Place: { en: "", ar: "" }, Image: "" },
+			{ Duration: "", Place: { en: "", ar: "" }, Image: "" },
+		],
+		Video: "",
+	},
 ];
